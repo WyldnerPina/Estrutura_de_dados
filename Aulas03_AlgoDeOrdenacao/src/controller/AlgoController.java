4c799ef2b6d331e5d8539868eec2b314891c1956
WyldnerPina <wyldner@gmail.com>
2022-08-28 12:40:45 -0300

    28/08/22 12:40

--- Aulas03_AlgoDeOrdenacao/src/controller/AlgoController.java
+++ Aulas03_AlgoDeOrdenacao/src/controller/AlgoController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import wyldner.BubbleSortClassic;
+import wyldner.MergeSortBasic;
+
 public class AlgoController {
 	public AlgoController() {
 		super();
@@ -7,22 +10,25 @@ public class AlgoController {
 
 // ================================== BUBLE SORT ======================================
 // ====================================================================================	
-	public int[] bubleSort(int[] num) {
-		
-		return num;
+	public int[] bubleSort(int[] vet) {
+		BubbleSortClassic bs = new BubbleSortClassic();
+		bs.bubbleSortClassic(vet);
+		return vet;
 	}
 
 // ================================== MERGE SORT ======================================
 // ====================================================================================	
-	public int[] mergeSort(int[] num) {
-
-		return num;
+	public int[] mergeSort(int[] vet) {
+		MergeSortBasic ms = new MergeSortBasic();
+		int tam = vet.length - 1;
+		ms.mergeSort(vet, 0, tam);
+		return vet;
 	}
 
 // ================================== QUICK SORT ======================================
 // ====================================================================================	
-//	public int[] quickSort(int[] num) {
+//	public int[] quickSort(int[] vet) {
 //
-//		return num;
+//		return vet;
 //	}
 }
