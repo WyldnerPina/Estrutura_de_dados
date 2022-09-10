9a6b0049ef867197a35f4e09814d4ace876b5428
WyldnerPina <wyldner@gmail.com>
2022-09-10 14:54:58 -0300

    10/09/22 14:54

--- Aula04_Pilha/Aula04_ExercNotacaoPolRev/src/controller/NPRController.java
+++ Aula04_Pilha/Aula04_ExercNotacaoPolRev/src/controller/NPRController.java
@@ -10,9 +10,36 @@ public class NPRController {
 	public void insereValor(int valor) {		
 		p.push(valor);
 	}
-
-	public int npr(String op, int n1, int n2) {
+	
+//=====================================================================================
+	public boolean isVazio() {
+		if(p.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int topo() throws Exception {
+		int topo;
+		topo = p.top();
+		return topo;
+	}
+	
+	public int tam() {
+		int tam;
+		tam = p.size();
+		return tam;
+	}
+	
+//=====================================================================================
+	public void npr(String op) {
 		int resultado = 0;
+		int n1 = 0, n2 = 0;
+		n2 = numPop(n2);
+		System.out.println("n2 " + n2);
+		n1 = numPop(n1);
+		System.out.println("n1 " + n1);
+		
 		switch (op) {
 		case "+":
 			resultado = n1 + n2;
@@ -30,6 +57,17 @@ public class NPRController {
 			JOptionPane.showMessageDialog(null, "Operação inválida");
 		}
 		System.out.println(n1 + op + n2 + " = " + resultado);
-		return resultado;
+		p.push(resultado);
+	}
+	
+//=====================================================================================	
+	public int numPop(int n) {
+		try {
+			n = p.pop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("pop de n " + n);
+		return n;
 	}
 }
--- Aula04_Pilha/Aula04_ExercNotacaoPolRev/src/view/Main.java
+++ Aula04_Pilha/Aula04_ExercNotacaoPolRev/src/view/Main.java
@@ -13,13 +13,29 @@ public class Main {
 		while(ctrl) {
 			String digitado = JOptionPane.showInputDialog("insira um número");
 			if(digitado.contains("+") || digitado.contains("-") || digitado.contains("*") || digitado.contains("/")){
-				nprc.npr(digitado, 10, 2);
-				ctrl = false;
+				if(nprc.tam() < 2) {
+					System.err.println("TAMANHO INSUFICIENTE ");
+				} else {
+					try {
+						nprc.npr(digitado);
+						ctrl = false;
+					}catch (NumberFormatException e) {
+						e.printStackTrace();
+					}					
+				}				
 			}else {
 				nprc.insereValor(Integer.parseInt(digitado));				
 			}						
 		}
+		
+//=========================================================================================================		
+		System.out.println("vazio: " + nprc.isVazio());		
+		System.out.println("tamanho: " + nprc.tam());		
+		try {
+			System.out.println("topoo: " + nprc.topo());			
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}
 //=========================================================================================================
 	}
-
 }
